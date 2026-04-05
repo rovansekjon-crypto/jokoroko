755faa519d61c4f1d27d5ef0f14d2414bb3d7a0f
rovansekjon-crypto <rovansekjon@gmail.com>
2026-04-05 15:49:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function downloadApp() {
-  window.location.href = "https://github.com/rovansekjon-crypto/jokoroko/releases/tag/v1.0";
+  window.location.href = "https://github.com/rovansekjon-crypto/jokoroko/releases/tag/v1.0/EliteCompressPro.exe";
 }
