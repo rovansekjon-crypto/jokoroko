52f78a7067b4fc7ac2320ade5b872a224f581157
rovansekjon-crypto <rovansekjon@gmail.com>
2026-04-06 16:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function downloadApp() {
-  window.location.href = "https://github.com/rovansekjon-crypto/jokoroko/releases/download/EliteCompressPro_Setup.exe";
+  window.location.href = "https://github.com/rovansekjon-crypto/jokoroko/releases/download/različicav2.0/EliteCompressPro_Setup.exe";
 }
